9c42b7131e457d09cc9732934260791bca8f20ca
Nitesh Mishra <mishra.nitesh987@gmail.com>
2020-04-21 21:22:11 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,49 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+
+const testData = [
+  {
+    name: "Dan Abramov",
+    avatar_url: "https://avatars0.githubusercontent.com/u/810438?v=4",
+    company: "@facebook",
+  },
+  {
+    name: "Sophie Alpert",
+    avatar_url: "https://avatars2.githubusercontent.com/u/6820?v=4",
+    company: "Humu",
+  },
+  {
+    name: "Sebastian Markbåge",
+    avatar_url: "https://avatars2.githubusercontent.com/u/63648?v=4",
+    company: "Facebook",
+  },
+];
+const CardList = (props) => {
+  return (
+    <div className="cardlist">
+      {testData.map((test) => (
+        <Card {...test} />
+      ))}
+    </div>
+  );
+};
+const Card = (props) => {
+  return (
+    <div className="card-component">
+      <img src={props.avatar_url} alt="pp" />
+      <div className="profile-info">
+        <div>{props.name}</div>
+        <div>{props.company}</div>
+      </div>
+    </div>
+  );
+};
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="header">The GitHub Cards App</div>
+      <CardList {...testData} />
     </div>
   );
 }
